b45733edcf738b9b7440e03a05e80824d429c4f2
Fernando Medina <fermmm@gmail.com>
2020-09-07 00:40:28 -0300

    Implemented sorting buttons

--- src/components/pages/FilmsList/FilmItem/FilmItem.tsx
+++ src/components/pages/FilmsList/FilmItem/FilmItem.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react';
+import moment from 'moment';
+import React, { FC, memo } from 'react';
 import { hot } from 'react-hot-loader';
 import { Film } from '../../../../api/tools/typings';
 import { Row } from '../../../common/Row/Row';
@@ -7,8 +8,8 @@ interface PropsFilmItem {
    filmData: Film;
 }
 
-export const FilmItem: FC<PropsFilmItem> = ({ filmData }) => {
-   return <Row>{filmData.title}</Row>;
-};
+export const FilmItem: FC<PropsFilmItem> = memo(({ filmData }) => {
+   return <Row>{filmData.title} {filmData.release_date}</Row>;
+});
 
 export default hot(module)(FilmItem);
--- src/components/pages/FilmsList/FilmsList.tsx
+++ src/components/pages/FilmsList/FilmsList.tsx
@@ -1,19 +1,42 @@
-import React, { FC } from 'react';
+import React, { FC, memo, useState, useEffect } from 'react';
 import { hot } from 'react-hot-loader';
+import { firstBy } from 'thenby';
 import { Film } from '../../../api/tools/typings';
 import { Column } from '../../common/Column/Column';
+import { Row } from '../../common/Row/Row';
 import FilmItem from './FilmItem/FilmItem';
 
 interface PropsFilmsList {
    films: Film[];
 }
 
-export const FilmsList: FC<PropsFilmsList> = ({ films }) => {
+export const FilmsList: FC<PropsFilmsList> = memo(({ films }) => {
+   const [filmsSorted, setFilmsSorted] = useState(films);
+   const [sortBy, setSortBy] = useState<keyof Film>();
+
+   useEffect(() => {
+      setFilmsSorted(sortList(films));
+   }, [films, sortBy]);
+
+   /**
+    * Here I could implement my own sort function but I decided to use 
+    * the "firstBy" module because in my opinion it's a better demonstration
+    * since this module is very powerful to implement very sophisticated 
+    * sorting logic in no time.
+    */
+   const sortList = (filmsUnsorted: Film[]) => 
+      filmsUnsorted && [...filmsUnsorted].sort(firstBy(f => f[sortBy]));
+
    return (
       <Column>
-         {films && films.map(filmData => <FilmItem key={filmData.episode_id} filmData={filmData} />)}
+         <Row>
+            Sort by:
+            <button onClick={() => setSortBy('title')}>Title</button>
+            <button onClick={() => setSortBy('release_date')}>Release Date</button>
+         </Row>
+         {filmsSorted && filmsSorted.map(filmData => <FilmItem key={filmData.episode_id} filmData={filmData} />)}
       </Column>
    );
-};
+});
 
 export default hot(module)(FilmsList);
